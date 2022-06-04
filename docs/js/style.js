fcb00694c46d3af24e7a1709a53d58c9ef7e6748
Grace Qiu <gracezqiu@gmail.com>
2022-06-04 13:43:00 -0400

    Added background images and cleaned up

--- docs/js/style.js
+++ docs/js/style.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
     // make page load smooth
     $("body").fadeIn(500);
+    window.onscroll = function() {onScroll()};
 });
+
+function onScroll() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      document.getElementById("navBar").style.background = "#07134F";
+    } else {
+      document.getElementById("navBar").style.background = "none";
+    }
+};
